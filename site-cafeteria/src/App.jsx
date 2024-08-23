f39d15ada52094a1cedee0940bc7a0ff7ee98104
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-08-23 15:30:16 -0300

    testing fonts

--- site-cafeteria/src/App.jsx
+++ site-cafeteria/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './styles/colorsAndFonts-module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  
+  
   return (
     <>
+    <main>
+      <h1>Teste</h1>
+      <h2>Teste</h2>
+      <h3>Teste</h3>
+
+    </main>
+    
    
     </>
   )
