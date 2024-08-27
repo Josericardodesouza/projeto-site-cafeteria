cf6f9d51fe4ec3f698d456f4c47d6ef6c07cb40d
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-08-27 11:02:41 -0300

    making to cofee list

--- site-cafeteria/src/App.jsx
+++ site-cafeteria/src/App.jsx
@@ -46,9 +46,64 @@ function App() {
       <section id='menu'>
 
         <div id='conteiner'>
+          <ol>
+
+            <li>
+              <label for = 'ping'>Pingado</label>
+              <input type='checkbox' id='ping' value = 'pingado'></input>
+            </li>
+
+
+            <li>
+              <label for = 'exp'>Espresso</label>
+               <input type='checkbox' id='exp' value = 'expresso'></input>
+              </li>
+
+              <li>
+                <label for = 'lat'>Latte</label>
+                <input type = 'checkbox' id = 'lat' value = 'latte'></input>
+              </li>
+
+              <li>
+                <label for = 'cap'>Cappucino</label>
+                <input type = 'checkbox' id='cap' value = 'cappucino'></input>
+              </li>
+
+              <li>
+                <label for = 'moc'>Mocha</label>
+                <input type = 'checkbox' id='moc' value = 'mocha'></input>
+              </li>
+
+              <li>
+                <label for = 'cbr'>Cold brew</label>
+                <input type = 'checkbox' id='cbr' value = 'cbrew'></input>
+              </li>
+
+              <li>
+                <label for = 'fap'>Fapuccino</label>
+                <input type="checkbox" id='fap' value = 'fapuccino'></input>
+              </li>
+
+              <li>
+                <label for = 'cge'>Café gelado</label>
+                <input type="checkbox" id='cge' value = 'caféGelado'/>
+              </li>
+
+              <li>
+                <label for = 'expt'>Expresso tônica</label>
+                <input type='checkbox' id='expt' value = 'expressoTonica'></input>
+              </li>
+
+              <li>
+                <label for = 'cfg'>Café gelado</label>
+                <input type = 'checkbox' id = 'cfg' value = 'cafeGelado'></input>
+              </li>
+
+           
+          </ol>
 
           <div id='box'>
-            <button></button>
+            <button>Seguir pedido</button>
 
           </div>
 
@@ -57,6 +112,7 @@ function App() {
       </section>
 
       <section id='evaluations'>
+        <h3>Avaliações dos nossos clientes</h3>
 
         {/* Swiper area */}
 
