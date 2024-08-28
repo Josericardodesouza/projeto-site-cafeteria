870a604f0770e27fc74d2fc85c99e92feab94c3c
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-08-28 11:07:59 -0300

    install react-icons

--- site-cafeteria/src/components/NavBar.jsx
+++ site-cafeteria/src/components/NavBar.jsx
@@ -1,15 +1,39 @@
 import {Link} from 'react-router-dom'
 import '../styles/navbar-modules.css'
+import { IoIosMenu } from 'react-icons/io'
+
 
 
 function NavBar() {
     return (
        
     <nav>
-      <a>Quem somos</a>
-      <a>Cardápio</a>
-      <a>Avaliações</a>
-      <Link to = '/about'>Contato</Link>
+      <div>
+        <a href="#">Cafeteria</a>
+      </div>
+
+
+      
+        <button>
+          <IoIosMenu size={40} />
+        </button>
+
+
+        <div id='menu_lateral'>
+        <h1>Teste lateral</h1>
+        <h1>Teste lateral</h1>
+
+
+        </div>
+      
+
+      <div id='box_links'>
+        <a>Quem somos</a>
+        <a>Cardápio</a>
+        <a>Avaliações</a>
+        <Link to = '/about'>Contato</Link>
+      </div>
+
     </nav>
 
     )
