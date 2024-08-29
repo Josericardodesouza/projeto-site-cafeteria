37de8b1e66113521579f1ab5cad1a40dc9d20145
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-08-29 10:21:46 -0300

    create Menu component

--- site-cafeteria/src/App.jsx
+++ site-cafeteria/src/App.jsx
@@ -46,9 +46,12 @@ function App() {
   
   return (
     <>
-    <NavBar />
+   
 
     <header>
+      
+      <NavBar />
+     
       
 
     </header>
@@ -74,75 +77,7 @@ function App() {
         </div>
 
       </section>
-      <section id='menu'>
-
-        <div id='conteiner'>
-          <ol>
-
-            <li>
-              <label for = 'ping'>Pingado</label>
-              <input type='checkbox' id='ping' value = 'pingado'></input>
-            </li>
-
-
-            <li>
-              <label for = 'exp'>Espresso</label>
-               <input type='checkbox' id='exp' value = 'expresso'></input>
-              </li>
-
-              <li>
-                <label for = 'lat'>Latte</label>
-                <input type = 'checkbox' id = 'lat' value = 'latte'></input>
-              </li>
-
-              <li>
-                <label for = 'cap'>Cappucino</label>
-                <input type = 'checkbox' id='cap' value = 'cappucino'></input>
-              </li>
-
-              <li>
-                <label for = 'moc'>Mocha</label>
-                <input type = 'checkbox' id='moc' value = 'mocha'></input>
-              </li>
-
-              <li>
-                <label for = 'cbr'>Cold brew</label>
-                <input type = 'checkbox' id='cbr' value = 'cbrew'></input>
-              </li>
-
-              <li>
-                <label for = 'fap'>Fapuccino</label>
-                <input type="checkbox" id='fap' value = 'fapuccino'></input>
-              </li>
-
-              <li>
-                <label for = 'cge'>Café gelado</label>
-                <input type="checkbox" id='cge' value = 'caféGelado'/>
-              </li>
-
-              <li>
-                <label for = 'expt'>Expresso tônica</label>
-                <input type='checkbox' id='expt' value = 'expressoTonica'></input>
-              </li>
-
-              <li>
-                <label for = 'cfg'>Café gelado</label>
-                <input type = 'checkbox' id = 'cfg' value = 'cafeGelado'></input>
-              </li>
-
-           
-          </ol>
-
-          <div id='box'>
-            <Link to = '/deliverypage'>
-            <button>Fazer pedido</button>
-            </Link>
-
-          </div>
-
-        </div>
-
-      </section>
+     
 
       <section id='evaluations'>
         <h3>Avaliações dos nossos clientes</h3>
