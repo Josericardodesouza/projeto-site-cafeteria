60e05007cf88ce8460541c965a7e604b73edd735
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-10-08 15:35:37 -0300

    update MenuCoffee

--- site-cafeteria/src/components/ConstantsItens.jsx
+++ site-cafeteria/src/components/ConstantsItens.jsx
@@ -1,13 +1,20 @@
 import cafeCoadoImg from '../assets/images/cards-images/cafe-coado.jpeg'
-import cafeGeladoImg from '../assets/images/cards-images/cafe-gelado.jpeg'
 import cappucinoImg from '../assets/images/cards-images/pexels-andrew-peterson-113438-350478.jpg'
-import coldBrewImg from '../assets/images/cards-images/cold-brew.jpg'
 import espressoImg from '../assets/images/cards-images/pexels-raymond-petrik-1448389535-28320963-espresso.jpg'
-import espressoTonicoImg from '../assets/images/cards-images/coffee-4862622_1280-espressotonico.jpg'
-import frappuccinoImg from '../assets/images/cards-images/cafe-frappuccino.jpeg'
 import latteImg from '../assets/images/cards-images/latte-art-2431160_1280-latte.jpg'
 import mochaImg from '../assets/images/cards-images/cafe-mocha.jpeg'
 import pingadoImg from '../assets/images/cards-images/pingado.jpg'
+
+
+import cafeGeladoImg from '../assets/images/cards-images/cafe-gelado.jpeg'
+
+import coldBrewImg from '../assets/images/cards-images/cold-brew.jpg'
+
+import espressoTonicoImg from '../assets/images/cards-images/coffee-4862622_1280-espressotonico.jpg'
+import frappuccinoImg from '../assets/images/cards-images/cafe-frappuccino.jpeg'
+
+
+
 
 
 import brigadeiroImg from '../assets/images/cards-images/pexels-livilla-latini-1678510737-27850066-brigadeiro.jpg'
@@ -24,7 +31,8 @@ const hotDrinks = [
             id: 1,
             name: 'café coado',
             price: '1,50',
-            img: cafeCoadoImg
+            img: cafeCoadoImg,
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, praesentium! Numquam minima aperiam libero dignissimos quam qui. Totam delectus vel ipsa illum exercitationem aperiam! Fuga officiis magnam voluptate nostrum deleniti!'
     
         },
 
@@ -32,7 +40,8 @@ const hotDrinks = [
             id: 2,
             name: 'Cappuccino',
             price: '6,00',
-            img: cappucinoImg
+            img: cappucinoImg,
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis atque doloribus tempora quidem nisi odio corrupti voluptas sit ut commodi? Voluptatum quaerat delectus nihil dicta maiores! Libero minima omnis enim. Cappucino'
     
         },
 
@@ -40,7 +49,8 @@ const hotDrinks = [
             id: 3,
             name: 'Espresso',
             price: '6,00',
-            img: espressoImg
+            img: espressoImg,
+            text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
         },
 
 
@@ -48,14 +58,16 @@ const hotDrinks = [
             id: 4,
             name: 'Latte',
             price: '20,00',
-            img: latteImg
+            img: latteImg,
+            text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
         },
 
         {
             id: 5,
             name: 'Mocha',
             price: '8,00',
-            img: mochaImg
+            img: mochaImg,
+            text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
         },
 
         
@@ -63,7 +75,8 @@ const hotDrinks = [
         id: 6,
         name: 'Pingado',
         price: '5,00',
-        img: pingadoImg
+        img: pingadoImg,
+        text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
     }
 
 
@@ -78,7 +91,8 @@ const coldDrinks = [
         id: 1,
         name: 'café gelado',
         price: '2,20',
-        img: cafeGeladoImg
+        img: cafeGeladoImg,
+        text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
     },
 
 
@@ -87,7 +101,8 @@ const coldDrinks = [
         id: 2,
         name: 'Cold Brew',
         price: '11,00',
-        img: coldBrewImg
+        img: coldBrewImg,
+        text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
 
     },
 
@@ -97,14 +112,16 @@ const coldDrinks = [
         id: 3,
         name: 'Expresso Tônico',
         price: '5,00',
-        img: espressoTonicoImg
+        img: espressoTonicoImg,
+        text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
     },
 
     {
         id: 4,
         name: 'Frappuccino',
         price: '19,00',
-        img: frappuccinoImg
+        img: frappuccinoImg,
+        text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
     }
 
 
@@ -125,42 +142,48 @@ const candyList = [
         id: 1,
         name: 'brigadeiros',
         price: '0,20',
-        img: brigadeiroImg
+        img: brigadeiroImg,
+        text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
     },
 
     {
         id: 2,
         name: 'Brownies',
         price: '3,00',
-        img: brownieImg
+        img: brownieImg,
+        text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
     },
 
     {
         id: 3,
         name: 'Cookies',
         price: '0,30',
-        img: cookieImg
+        img: cookieImg,
+        text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
     },
 
     {
         id: 4,
         name: 'Cupcakes',
         price: '7,00',
-        img: cupcakeImg
+        img: cupcakeImg,
+        text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
     },
 
     {
         id: 5,
         name: 'Pâo de Mel',
         price: '1,50',
-        img: paoDeMelImg
+        img: paoDeMelImg,
+        text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
     },
 
     {
         id: 6,
         name: 'Tortas',
         price: '16,00',
-        img: tortaImg
+        img: tortaImg,
+        text: <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis omnis atque fuga, dolores repudiandae. Quas inventore ab doloremque vitae at nostrum quo nobis maiores culpa et excepturi, veritatis modi.</p>
     }
 ]
 
--- site-cafeteria/src/components/MenuCoffee.jsx
+++ site-cafeteria/src/components/MenuCoffee.jsx
@@ -1,12 +1,93 @@
 import '../styles/menus-module.css'
 
 
+import cafeCoadoImg from '../assets/images/cards-images/cafe-coado.jpeg'
+import cappucinoImg from '../assets/images/cards-images/pexels-andrew-peterson-113438-350478.jpg'
+import espressoImg from '../assets/images/cards-images/pexels-raymond-petrik-1448389535-28320963-espresso.jpg'
+import latteImg from '../assets/images/cards-images/latte-art-2431160_1280-latte.jpg'
+import mochaImg from '../assets/images/cards-images/cafe-mocha.jpeg'
+import pingadoImg from '../assets/images/cards-images/pingado.jpg'
+
+
 
 import { hotDrinks } from './ConstantsItens'
+import { useRef } from 'react'
+
+const coado = hotDrinks[0]
+const cappuccino = hotDrinks[1]
+const espresso = hotDrinks[2]
+const latte = hotDrinks[3]
+const mocha = hotDrinks[4]
+const pingado = hotDrinks[5]
+
+
+
 
 
 
 function MenuCoffee() {
+
+
+    const nameRef = useRef(null)
+    
+
+
+    const imageCoffee = document.getElementById('item_img')
+    var description = document.getElementById('item_text')
+    var texto = ''
+  
+
+    const rodar = (event) => {
+
+        const nameItem = event.target.textContent
+
+        switch (nameItem) {
+            case 'café coado':
+                console.log('teste coado')
+                
+                document.getElementById('item_text').innerHTML = `${coado.text}`
+                imageCoffee.src =  coado.img
+                
+                console.log(coado.text)
+                break;
+
+            case 'Cappuccino':
+                console.log('teste cappuccino')
+                document.getElementById('item_text').innerHTML = `${cappuccino.text}`
+                imageCoffee.src = cappuccino.img
+
+                console.log(cappuccino.text)
+                break;
+
+            case 'Espresso':
+                console.log('teste espresso')
+                document.getElementById('item_text').innerHTML = `${espresso.text}`
+                imageCoffee.src = espresso.img
+
+                break;
+            
+            case 'Latte':
+                console.log('teste latte')
+                document.getElementById('item_text').innerHTML = `${latte.text}`
+                imageCoffee.src = latte.img
+
+                break;
+
+            case 'Mocha':
+                console.log('teste Mocha')
+                document.getElementById('item_text')
+            
+            default:
+                console.log('não passou')
+        }
+
+
+        
+
+
+    }
+
+
 
 
 
@@ -18,17 +99,61 @@ function MenuCoffee() {
        <section className='section_menu'>
 
         <div className = 'box_menu'>
-        <h1 className = 'title_table'>Quentes</h1> 
+   
+
+        <div>
+            <img src= "" alt="" id='item_img' />
+            <p id='item_text'></p>
+        </div>
+
+
+
+
+        <div id='imgCoffee'></div>
             <table className='cardapio'>
                 <tbody>
-                    {hotDrinks.map(cafes => (
-                        <tr key={cafes.id}>
-                            <td className='item'>{cafes.name}</td>
-                            <td className='value'>R$ {cafes.price}</td>
+                    
+                        <tr>
+                            <td className='item'><a onClick= {rodar}>{coado.name}</a></td>
+                            <td className='value'><a onClick="">{coado.price}</a></td>
                         </tr>
-                    ))}
+
+                        <tr>
+                            <td className='item'><a onClick= {rodar}>{cappuccino.name}</a></td>
+                            <td className='value'><a onClick="">{cappuccino.price}</a></td>
+                        </tr>
+
+                        <tr>
+                            <td className='item'><a onClick="">{espresso.name}</a></td>
+                            <td className='value'><a onClick="">{espresso.price}</a></td>
+                        </tr>
+
+                        <tr>
+                            <td className='item'><a onClick="">{latte.name}</a></td>
+                            <td className='value'><a onClick="">{latte.price}</a></td>
+                        </tr>
+
+                        <tr>
+                            <td className='item'><a onClick="">{mocha.name}</a></td>
+                            <td className='value'><a onClick="">{latte.price}</a></td>
+                        </tr>
+
+                        <tr>
+                            <td className='item'><a onClick="">{pingado.name}</a></td>
+                            <td className='value'><a onClick="">{pingado.price}</a></td>
+                        </tr>
+
+
+                
+
+                
                     </tbody>
+
+
+                    
             </table>
+
+         
 
         </div>
 
