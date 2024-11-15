d4cf3f6fb1100aaa926a93a2056a2075ea759f81
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-11-15 14:23:53 -0300

    Update UserComment.jsx

--- site-cafeteria/src/components/UserComment.jsx
+++ site-cafeteria/src/components/UserComment.jsx
@@ -1,13 +1,43 @@
+import { useState } from "react"
+
 function UserComment() {
+
+    const [comment, setComment] = useState('')
+
+    const [storedComment, setStoredComment] = useState('')
+
+
+    const textChange = (e) => {
+        setComment(e.target.value)
+
+
+    }
+
+    const confirmComment = (e) => {
+        setStoredComment(comment)
+    }
+
+    
 
     return (
         <>
 
-        <textarea placeholder="teste"></textarea>
+        <textarea placeholder="teste" onChange={textChange}></textarea>
+
+        <button onClick={confirmComment}>Confirmar</button>
+
+        <div>
+            <p>comentário:</p>
+            {storedComment}
+        </div>
+
+        
         
         </>
     )
 
 }
+
+
 
 export default UserComment
